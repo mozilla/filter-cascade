7011b21ae07b602a498fabce13fc654ee97aedab
Mark Goodwin <mgoodwin@mozilla.com>
2019-05-03 15:25:49 +0100

    moved module code from filtercascade.py to filtercascade/__init__.py because of packaging woes, updated .gitignore for the build directory

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(name='filtercascade',
-      version='0.1',
+      version='0.1.1',
       description='A simple bloom filter cascade implementation in Python',
       long_description='A bloom filter cascade implementation in Python using the 32-bit variant of murmurhash3.',
       classifiers=[
         'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: MPL-2.0',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
         'Programming Language :: Python :: 3',
-        'Topic :: Software Development :: Data Structure',
       ],
       keywords='bloom filter cascade multi level mlbf',
       url='http://github.com/mozmark/filter-cascade',
       author='Mark Goodwin',
       author_email='mgoodwin@mozilla.com',
-      license='MPL-2.0',
-      packages=find_packages(),
+      license='Mozilla Public License 2.0 (MPL 2.0)',
+      packages=['filtercascade'],
       install_requires=[
           'bitarray>=0.9.2',
           'mmh3>=2.5.1',
