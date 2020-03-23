8f2356a72d6d9301a71eb07bb488e288211070e8
J.C. Jones <jc@mozilla.com>
2020-03-23 16:07:01 -0700

    Fix #12 - Duplicated hash algorithm serialization

---
+++ filtercascade/fileformats.py
@@ -0,0 +1,28 @@
+import struct
+
+# The header for each Bloom filter level
+# Little endian (<)
+# byte 0: Hash algorithm enumeration, as an unsigned char
+# bytes 1-4: L, length of the bitarray, in bytes, as an unsigned int
+# bytes 5-8: the number of hash functions for this layer, as an unsigned int
+# byte 9: The layer number of this bloom filter, as an unsigned char
+bloomer_struct = struct.Struct(b"<BIIB")
+
+# This struct packs the hash iteration number and the layer number
+# into two bytes in a defined way for SHA256.
+# byte 0: hash iteration number as an unsigned char
+# byte 1: layer number of this bloom filter, as an unsigned char
+bloomer_sha256_hash_struct = struct.Struct(b"<BB")
+
+# The version struct is a simple 2-byte short indicating version number
+# Little endian (<)
+# bytes 0-1: The version number of this filter, as an unsigned short
+version_struct = struct.Struct(b"<H")
+
+# version 2 filters, after the version_struct field, have
+# the following:
+# Little endian (<)
+# byte 0: Whether the decision logic should be inverted, as a boolean
+# byte 1: L, length of salt field as a unsigned char
+# bytes 2+: salt field of length L
+version_2_salt_struct = struct.Struct(b"<?B")
