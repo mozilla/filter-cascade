ff01471b4c6a5815b6954d9c46406c37d01c43da
J.C. Jones <jc@mozilla.com>
2020-03-27 17:10:08 -0700

    Bump to 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="filtercascade",
-    version="0.2.2",
+    version="0.3.0",
     description="A simple bloom filter cascade implementation in Python",
     long_description="A bloom filter cascade implementation in Python using the 32-bit variant of murmurhash3.",
     classifiers=[
