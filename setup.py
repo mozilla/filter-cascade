179b90ed12804659dd3350a145bf1a845414eb92
J.C. Jones <jc@mozilla.com>
2020-04-10 08:21:51 -0700

    Update setup.py description and author

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="filtercascade",
     version="0.4.0",
     description="A simple bloom filter cascade implementation in Python",
-    long_description="A bloom filter cascade implementation in Python using the 32-bit variant of murmurhash3.",
+    long_description="A bloom filter cascade implementation in Python using either the 32-bit variant of murmurhash3 or sha256.",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
@@ -17,11 +17,11 @@ setup(
         "Source": "https://github.com/mozilla/filter-cascade",
         "Tracker": "https://github.com/mozilla/filter-cascade/issues",
     },
-    author="Mark Goodwin",
-    author_email="mgoodwin@mozilla.com",
+    author="J.C. Jones",
+    author_email="jc@mozilla.com",
     license="Mozilla Public License 2.0 (MPL 2.0)",
     packages=["filtercascade"],
-    install_requires=["bitarray>=0.9.2", "mmh3>=2.5.1",],
+    install_requires=["bitarray>=0.9.2", "mmh3>=2.5.1", "Deprecated"],
     include_package_data=True,
     zip_safe=False,
 )
